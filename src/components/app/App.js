cf201935cb55b3f03d833ce77dcea0eb9e33a54e
Zakhar <uvanov977777@gmail.com>
2023-03-07 17:52:09 +0300

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,12 @@
+import AppHeaderMainPage from '../appHeaderMainPage/app-header-main-page';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeaderMainPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/appHeaderMainPage/app-header-main-page.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './app-header-main-page.scss'
+import logo from '../../resources/img/logo.svg';
+import email from '../../resources/img/E-mail.svg'
+
+
+const AppHeaderMainPage = () => {
+    return (
+        <div className="app-header__main-page">
+            <div className="container">
+                <img src={logo} className="logo" alt="logo"/>
+                <div className='address'>
+                    <div className='col-left__address'> 
+                       <img src={email} className="email_ico" alt="email"/>
+                    </div>
+                    <div className='col-right__address'> 
+                        <a href="mailto:6220095@mail.ru">6220095@mail.ru</a>
+                        <p>Мы на связи!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AppHeaderMainPage;
